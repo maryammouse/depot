e09735835764b1b00507e9516befee89728a813b
Maryam Syed <maryamsyed2096@gmail.com>
2014-11-18 11:46:56 -0800

    made heroku app

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,15 @@ class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
+  validates :title, length: { minimum: 10 }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
-    message: 'must be a URL for GIF, JPG, or PNG image.'
+    message: 'must end in .GIF, .JPG, or .PNG'
   }
 end
+
+def self.latest
+  Product.order(:updated_at).last
+end 
 
 
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -57,6 +57,4 @@ class ProductTest < ActiveSupport::TestCase
     assert product.invalid?
     assert_equal ["has already been taken"], product.errors[:title]
   end
-
-  
 end
