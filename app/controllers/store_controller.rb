1463b2df84ddb0acf8194f5a913d99fb10374af1
Maryam Syed <maryamsyed2096@gmail.com>
2014-11-19 12:30:07 -0800

    Add cart to app

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,12 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @time = Time.now()
+    @counter = session[:counter]
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+    end
+    
   end
 end
